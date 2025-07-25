43541bbfc9fb1638d3242cfa90350485f9c9c75e
AngelaRebancosBautista <angelarbautista22@gmail.com>
2025-07-25 21:53:13 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratory_Activity_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check if it's even or odd:");
+            string input = Console.ReadLine();
+
+            if (!int.TryParse(input, out int number))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+                return;
+            }
+            if (number % 2 == 0)
+            {
+                Console.WriteLine($"The number {number} is even.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is odd.");
+            }
+        }
+    }
+}
+         
+
+
+
+
+
+
+
+
